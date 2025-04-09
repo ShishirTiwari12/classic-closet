7fd3f64c97c09763f80cc7ea319fa03056b56f4b
ShishirTiwari12 <shishir18028@gmail.com>
2025-04-09 20:40:14 +0545

    revised for presentatation

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,3 +1,5 @@
+
+
 const isAuthenticated = async (req,res,next)=>{
     if(!req.session.user){
         return res.redirect('http://localhost:3000/auth.html');
--- utils/mailer.js
+++ utils/mailer.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 // Set up the transporter using Gmail's SMTP server
 const transporter = nodemailer.createTransport({
   service: 'gmail', 
   auth: {
-    user: 'edummy142@gmail.com', // Replace with your Gmail address
-    pass: 'oxbj kauj njcw gnhg'    // Replace with your Gmail password or App Password
+    user: process.env.MAIL_ID, // Replace with your Gmail address
+    pass: process.env.MAIL_PASS    // Replace with your Gmail password or App Password
   }
 });
 
@@ -14,7 +15,7 @@ const sendVerificationEmail = async (toEmail, token) => {
   const verificationLink = `http://localhost:3000/auth/verify-email?token=${token}`;
 
   const mailOptions = {
-    from: 'edummy142@gmail.com',  // Sender's email
+    from: process.env.MAIL_ID,  // Sender's email
     to: toEmail,  // Recipient's email
     subject: 'Verify Your Email Address',
     text: `Please click the link below to verify your email address:\n\n${verificationLink}`
